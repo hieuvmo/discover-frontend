5fff9ce955ccdcb153e29751aa821c405990573e
Viết Minh Hiếu <hieuvm1@vmodev.com>
2022-11-24 15:53:11 +0700

    feat: setup tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "576px",
+        "sm-max": { max: "576px" },
+        md: "768px",
+        "md-max": { max: "768px" },
+        lg: "992px",
+        "lg-max": { max: "992px" },
+        xl: "1200px",
+        "xl-max": { max: "1200px" },
+        "2xl": "1320px",
+        "2xl-max": { max: "1320px" }
+      },
+      colors: {
+        primary: "#7F56D9"
+      }
+    }
+  },
+  plugins: []
+};
