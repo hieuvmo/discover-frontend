405f99b7adaa4221d6099d5d5835f9646a68866d
Viết Minh Hiếu <hieuvm1@vmodev.com>
2023-01-13 10:50:45 +0700

    feat: create laptop overview ui

--- src/modules/LaptopDetail/LaptopDetail.styled.ts
+++ src/modules/LaptopDetail/LaptopDetail.styled.ts
@@ -150,3 +150,42 @@ export const StoreCommitmentOrderList = styled.div`
     }
   }
 `;
+
+export const LaptopOverviewContainer = styled.div`
+  font-size: 1rem;
+
+  img {
+    border-radius: 0.5rem;
+  }
+`;
+
+export const LaptopOverviewSeeMore = styled.div`
+  width: 100%;
+  height: 9rem;
+  background: linear-gradient(
+    180deg,
+    hsla(0, 0%, 100%, 0),
+    hsla(0, 0%, 100%, 0.91) 50%,
+    #f9f9f9 55%
+  );
+  position: absolute;
+  bottom: 0;
+
+  button {
+    position: absolute;
+    bottom: 1rem;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 9.5rem;
+    display: flex !important;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 2rem;
+
+    button:hover {
+      fill: #a481e6;
+    }
+  }
+`;
--- src/modules/LaptopDetail/components/LaptopOverview.tsx
+++ src/modules/LaptopDetail/components/LaptopOverview.tsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+import { useAppSelector } from "hooks/useRedux";
+import { RootState } from "redux/store";
+import { CommonButton } from "components";
+import { ChevronDownIcon } from "icons";
+import {
+  LaptopOverviewContainer,
+  LaptopOverviewSeeMore
+} from "../LaptopDetail.styled";
+
 const LaptopOverview = () => {
-  return <div>LaptopOverview</div>;
+  const { t } = useTranslation();
+  const [isShowAll, setIsShowAll] = useState<boolean>(false);
+  const { laptopDetail } = useAppSelector((state: RootState) => state.laptop);
+
+  const handleClickShowAllOverview = () => {
+    setIsShowAll(true);
+  };
+
+  return (
+    <>
+      <LaptopOverviewContainer
+        className={`${!isShowAll ? "h-[50rem] overflow-hidden" : ""}`}
+        dangerouslySetInnerHTML={{ __html: `${laptopDetail?.review}` }}
+      />
+      {!isShowAll && (
+        <LaptopOverviewSeeMore>
+          <CommonButton
+            type="default"
+            content={t("laptop:see_more")}
+            icon={<ChevronDownIcon width={16} />}
+            onClick={handleClickShowAllOverview}
+          />
+        </LaptopOverviewSeeMore>
+      )}
+    </>
+  );
 };
 
 export default LaptopOverview;
